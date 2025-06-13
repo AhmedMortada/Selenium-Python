fea5dfe848de3a7a9877764b589c2fdf06203952
Mahmoud <ahmed.abdelfattah@accenture.com>
2025-06-13 17:09:53 +0300

    Test

--- features/steps/test_login.py
+++ features/steps/test_login.py
@@ -5,13 +5,13 @@ scenarios('../login.feature')
 
 @given('the user is on the login page')
 def go_to_login_page(driver):
-    driver.get("https://example.com/login")
+    driver.get("https://www.google.com/")
 
-@when('the user enters valid credentials')
-def enter_credentials(driver):
-    login_page = LoginPage(driver)
-    login_page.login('user', 'pass')
+# @when('the user enters valid credentials')
+# def enter_credentials(driver):
+#     login_page = LoginPage(driver)
+#     login_page.login('user', 'pass')
 
-@then('the user should be redirected to the dashboard')
-def verify_login(driver):
-    assert "dashboard" in driver.current_url
+# @then('the user should be redirected to the dashboard')
+# def verify_login(driver):
+#     assert "dashboard" in driver.current_url
